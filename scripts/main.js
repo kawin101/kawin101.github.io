95c3b292eba23c9865cf6f565042767679a6fa6e
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-01-06 11:24:48 +0700

    [Add] Dynamic Year in Footer

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,8 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'G-5T2PQELT0T');
+
+// Add dynamic year to footer
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('year').textContent = new Date().getFullYear();
+});
