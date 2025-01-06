e7aad59476ceec8da270064662752ba6d91dd761
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-01-06 11:27:22 +0700

    Remove Don't work for real-time

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,3 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'G-5T2PQELT0T');
-
-// Add dynamic year to footer
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementById('year').textContent = new Date().getFullYear();
-});
