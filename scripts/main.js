ca3928393fe1104bb4b41a1b8524b305a8010447
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-01-28 02:40:59 +0700

    up

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,20 @@ function gtag() {
 }
 gtag('js', new Date());
 gtag('config', 'G-5T2PQELT0T');
+
+document.addEventListener('DOMContentLoaded', function() {
+  var countdownElement = document.getElementById('countdown');
+
+  function updateCountdown() {
+    var now = new Date();
+    var options = { timeZone: 'Asia/Bangkok', hour12: false };
+    var bangkokTime = now.toLocaleString('en-GB', options);
+    countdownElement.textContent = bangkokTime;
+  }
+
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+
+  var welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+  welcomeModal.show();
+});
